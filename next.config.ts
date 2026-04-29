d88599a28ff48bc77a3e3e16b09423d19ca3b521
Prince Seth <prince11mayminote7pro@gmail.com>
2026-04-30 01:43:44 +0530

    Fix next.config.ts type error

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,7 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+  // @ts-ignore - 'eslint' does not exist in type 'NextConfig' according to strict types, but is still parsed by Next.js
   eslint: {
     ignoreDuringBuilds: true
   }
